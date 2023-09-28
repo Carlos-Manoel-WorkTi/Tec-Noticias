baf4b4f195cf2c09f1d5f178b7de01f2e8a0d00f
Joabe Nascimento <joabe-nascimento>
2023-09-28 01:35:40 -0300

    navbar adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Router>
+    <Router>  
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
